9d1d255e3c54a082ed118f206c79232d2e67a32c
Timm Liberty <timmliberty@timms-imac.local>
2016-12-06 12:58:09 -0500

    feedUI complete. Next hooking up cell

--- Social App/CircleView.swift
+++ Social App/CircleView.swift
@@ -1,18 +1,18 @@
 //
-//  FancyView.swift
-//  MySocialMediaApp
+//  CircleView.swift
+//  Social App
 //
-//  Created by Timm Liberty on 11/27/16.
+//  Created by Timm Liberty on 12/6/16.
 //  Copyright © 2016 Briantiumapps. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
 
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         
         layer.shadowOpacity = 0.8
@@ -21,7 +21,13 @@ class FancyView: UIView {
         
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
         
-        layer.cornerRadius = 2.0
+    }
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        layer.cornerRadius = self.frame.width / 2
+
     }
 
 }
