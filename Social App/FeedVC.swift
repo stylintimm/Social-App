a6c55fb2a88b79d84924c3249e9ea7809d14bfaf
Timm Liberty <timmliberty@timms-imac.local>
2016-12-08 06:23:01 -0500

    Completed get requests to firebase

--- Social App/FeedVC.swift
+++ Social App/FeedVC.swift
@@ -20,6 +20,13 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         // Do any additional setup after loading the view.
         tableView.delegate = self
         tableView.dataSource = self
+        
+        DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
+            print(snapshot.value!)
+            
+            
+        })
+    
     }
     
     
